01bff39cd2cbe25800ed46d488d769232beff6af
luji <chl3188@gmail.com>
2024-07-22 14:55:35 +0900

    스프링 데이터 Common 기본 리포지토리 커스터마이징

--- src/main/java/me/luji/study/post/PostRepository.java
+++ src/main/java/me/luji/study/post/PostRepository.java
@@ -1,7 +1,7 @@
 package me.luji.study.post;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import me.luji.study.MyRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long>, PostCustomRepository<Post> {
+public interface PostRepository extends MyRepository<Post, Long> {
 
 }
